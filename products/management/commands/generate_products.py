7d66eb170e65a99ec3abc26e2a36facf66e4b194
Kyrylo Malyshev <kirilm172@gmail.com>
2022-03-21 15:32:29 +0200

    using bulk

--- products/management/commands/generate_products.py
+++ products/management/commands/generate_products.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.core.management.base import BaseCommand
 from products.models import Category, Product
 import random
@@ -13,19 +14,20 @@ class Command(BaseCommand):
         cc = kwargs['cc']
         pc = kwargs['pc']
 
-        for category in Category.objects.all():
-            category.delete()
+        Category.objects.all().delete()
+        Product.objects.all().delete()
 
-        for product in Product.objects.all():
-            product.delete()
+        categories = Category.objects.bulk_create([Category(name=f'category{i + 1}') for i in range(cc)])
 
-        Category.objects.bulk_create([Category(name=f'category{i + 1}') for i in range(cc)])
-
-        for i, category in enumerate(Category.objects.all()):
-            Product.objects.bulk_create([Product(
-                name=f'product{(i + 1)*pc + j}',
-                category = category,
-                price = random.randint(0, 10000)/100,
-                status = random.choice(['in_stock', 'out_of_stock']),
-                remains = random.randint(0, 100)
-            ) for j in range(pc)])
+        products_to_create = []
+        for i, category in enumerate(categories):
+            for j in range(pc):
+                product = Product(
+                    name=f'product{(i + 1)*pc + j}',
+                    category = category,
+                    price = random.randint(0, 10000)/100,
+                    status = random.choice(['in_stock', 'out_of_stock']),
+                    remains = random.randint(0, 100)
+                )
+                products_to_create.append(product)
+        Product.objects.bulk_create(products_to_create)
--- products/management/commands/randomize_products.py
+++ products/management/commands/randomize_products.py
@@ -1,3 +1,4 @@
+from math import prod
 from django.core.management.base import BaseCommand
 from products.models import Product
 
@@ -8,8 +9,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
+        products_to_update = []
         for product in Product.objects.all():
             product.price = random.randint(0, 10000)/100
             product.status = random.choice(['in_stock', 'out_of_stock'])
             product.remains = random.randint(0, 100)
-            product.save()
+            products_to_update.append(product)
+
+        Product.objects.bulk_update(products_to_update, fields=['price', 'status', 'remains'])
